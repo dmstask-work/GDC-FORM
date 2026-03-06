6793ed4fccf07be0240b52b02cb39b4c893016e2
dmstask-work <rs.iki.dimas@gmail.com>
2026-03-06 16:07:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://vdlbwrzfmwhblcnoajsc.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkbGJ3cnpmbXdoYmxjbm9hanNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0NjEyMTcsImV4cCI6MjA4NzAzNzIxN30.Z4pZnnrWALOpvLx4gP0nEoUckKJf9Ja43Y3P--TZcIs",
+  TABLE_NAME: "customer_submissions",
+  STORAGE_BUCKET: "customer-photos",
+  // Set this to your deployed edge function endpoint.
+  // Example: https://<project-ref>.functions.supabase.co/reverse-geocode
+  REVERSE_GEOCODE_URL: ""
+};
